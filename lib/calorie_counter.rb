1897a5929e1a5c8f9502149a883557a56855b06a
Sarah Hall <swatson1994@yahoo.com>
2022-12-01 12:25:26 -0500

    Add tests for read_list, translate_list, and hcc, and methods

--- lib/calorie_counter.rb
+++ lib/calorie_counter.rb
@@ -1,7 +1,10 @@
 require 'pry'
 
 class CalorieCounter
+
+  attr_reader :breakdown
   def initialize
+    @breakdown = {}
   end
 
   def read_list(file_loc)
@@ -9,5 +12,19 @@ class CalorieCounter
     file_data = file.read
     file.close
     file_data.split("\n\n")
+  end
+
+  def translate_list(file_loc)
+    x = read_list(file_loc)
+    y = x.map { |e| e.split("\n")}
+    y.each_index do |index|
+      @breakdown["elf-#{index}"] = y[index].map { |num| num.to_i}.sum
+    end
+    return @breakdown
+  end
+
+  def highest_calorie_count(file_loc)
+    translate_list(file_loc)
+    @breakdown.values.max
   end
 end
--- spec/calorie_count_spec.rb
+++ spec/calorie_count_spec.rb
@@ -7,10 +7,26 @@ describe CalorieCounter do
     elf_list = CalorieCounter.new
     expect(elf_list).to be_an_instance_of(CalorieCounter)
   end
+
   it 'can read from a txt file into an array' do
     elf_list = CalorieCounter.new
     x = elf_list.read_list('./elf_calories/test_calorie_list.txt')
     expect(x).to be_an(Array)
     expect(x.length).to eq(5)
+  end
+
+  it 'can reformat read array into a hash of sums, and store' do
+    elf_list = CalorieCounter.new
+    x = elf_list.translate_list('./elf_calories/test_calorie_list.txt')
+    expect(x).to be_a(Hash)
+    expect(x["elf-0"]).to eq(6000)
+    expect(elf_list.breakdown).to be_a(Hash)
+    expect(elf_list.breakdown.empty?).to be(false)
+  end
+
+  it 'can return the highest sum from breakdown list' do
+    elf_list = CalorieCounter.new
+    highest_sum = elf_list.highest_calorie_count('./elf_calories/test_calorie_list.txt')
+    expect(highest_sum).to eq(24000)
   end
 end
