ddde069c25bcf58f7e3f7ba9d74dcd6fe11947e4
Sarah Hall <swatson1994@yahoo.com>
2022-12-02 21:28:45 -0500

    Add p2 tally method test, and make pass

--- rock_paper_scissors/lib/RPS.rb
+++ rock_paper_scissors/lib/RPS.rb
@@ -2,10 +2,11 @@ require 'pry'
 
 class RockPaperScissors
 
-  attr_reader :play_pat
+  attr_reader :play_pat, :player_two_score
 
   def initialize(file_loc)
     @play_pat = format_play_pat(file_loc)
+    @player_two_score = 0
   end
 
   def format_play_pat(file_loc)
@@ -13,5 +14,23 @@ class RockPaperScissors
     file_data = file.read
     file.close
     file_data.split("\n")
+  end
+
+  def player_two_score_tally
+    @play_pat.each do |string|
+      if string[0] == "A"
+        @player_two_score += 4 if string[2] == "X"
+        @player_two_score += 8 if string[2] == "Y"
+        @player_two_score += 3 if string[2] == "Z"
+      elsif string[0] == "B"
+        @player_two_score += 1 if string[2] == "X"
+        @player_two_score += 5 if string[2] == "Y"
+        @player_two_score += 9 if string[2] == "Z"
+      else
+        @player_two_score += 7 if string[2] == "X"
+        @player_two_score += 2 if string[2] == "Y"
+        @player_two_score += 6 if string[2] == "Z"
+      end
+    end
   end
 end
--- rock_paper_scissors/spec/RPS_spec.rb
+++ rock_paper_scissors/spec/RPS_spec.rb
@@ -7,9 +7,17 @@ describe RockPaperScissors do
     game = RockPaperScissors.new("./rock_paper_scissors/test_rps_pattern.txt")
     expect(game).to be_an_instance_of(RockPaperScissors)
   end
+
   it 'can format a play pattern' do
     game = RockPaperScissors.new("./rock_paper_scissors/test_rps_pattern.txt")
     expect(game.play_pat).to be_an(Array)
+    expect(game.play_pat.count).to eq(3)
+  end
+
+  it 'can tally player 2s score' do
+    game = RockPaperScissors.new("./rock_paper_scissors/test_rps_pattern.txt")
+    game.player_two_score_tally
+    expect(game.player_two_score).to eq(15)
   end
 end
 
