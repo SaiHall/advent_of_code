a5803f8e950ef3c7711caf5cf5be26959a35eaed
Sarah Hall <swatson1994@yahoo.com>
2022-12-05 08:44:01 -0500

    Add test for desired formatting of txt files, make pass

--- rucksack_reo/lib/locate_dupes.rb
+++ rucksack_reo/lib/locate_dupes.rb
@@ -2,9 +2,25 @@ require 'pry'
 
 class RucksackReo
 
-  attr_reader :duplicates
+  attr_reader :duplicates, :formatted_list
   def initialize
     @duplicates = []
+    @formatted_list = []
   end
 
+  def read_file_in(file_loc)
+    file = File.open(file_loc)
+    file_data = file.read
+    file.close
+    file_data.split("\n")
+  end
+
+  def format_file(file_loc)
+    content_ary = read_file_in(file_loc)
+    content_ary.each do |e|
+      start_num = e.length / 2
+      end_num = start_num - 1
+      @formatted_list.push([e[0..end_num], e[start_num..-1]])
+    end
+  end
 end
--- rucksack_reo/spec/locate_dupes_spec.rb
+++ rucksack_reo/spec/locate_dupes_spec.rb
@@ -3,13 +3,20 @@ require 'Rspec'
 require_relative '../lib/locate_dupes'
 
 describe RucksackReo do
+  before(:each) do
+    @bags = RucksackReo.new
+    @test_file = './rucksack_reo/test_rucksack_list.txt'
+    @file = './rucksack_reo/rucksack_list.txt'
+  end
   it 'exists and has attributes' do
-    bags = RucksackReo.new
-    expect(bags).to be_an_instance_of(RucksackReo)
-    expect(bags.duplicates).to eq([])
+    expect(@bags).to be_an_instance_of(RucksackReo)
+    expect(@bags.duplicates).to eq([])
   end
 
   it 'can format the rucksack list into a usable format' do
-    
+    @bags.format_file(@test_file)
+    formatted_file_data = @bags.formatted_list
+    expect(formatted_file_data).to be_an(Array)
+    expect(formatted_file_data[1]).to be_an(Array)
   end
 end
