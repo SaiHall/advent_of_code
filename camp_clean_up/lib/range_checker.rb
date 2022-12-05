802919ca5cb8bcc57bcd721b1e4581b10b47f2e0
Sarah Hall <swatson1994@yahoo.com>
2022-12-05 11:13:12 -0500

    Add test for formatting of pairs, make pass

--- camp_clean_up/lib/range_checker.rb
+++ camp_clean_up/lib/range_checker.rb
@@ -2,10 +2,28 @@ require 'pry'
 
 class RangeChecker
 
+  attr_reader :pair_list
   def initialize
+    @pair_list = []
   end
 
   def contained?(array)
     array[0].include?(array[1]) || array[1].include?(array[0])
+  end
+
+  def populate_pairs(file_loc)
+    file = File.open(file_loc)
+    file_data = file.read
+    file.close
+    @pair_list = file_data.split("\n")
+  end
+
+  def format_pair_list(file_loc)
+    populate_pairs(file_loc)
+    @pair_list = @pair_list.map do |e|
+      e.split(',').map do |f|
+        ((f[0].to_i)..(f[2].to_i)).to_a.join
+      end
+    end
   end
 end
--- camp_clean_up/spec/range_checker_spec.rb
+++ camp_clean_up/spec/range_checker_spec.rb
@@ -1,21 +1,31 @@
 require 'pry'
 require 'Rspec'
-require_relative '../lib/range_checker.rb'
+require_relative '../lib/range_checker'
 
 describe RangeChecker do
+  before(:each) do
+    @checker = RangeChecker.new
+    @file = './camp_clean_up/ranges.txt'
+    @test = './camp_clean_up/test_ranges.txt'
+  end
   it 'exists' do
-    checker = RangeChecker.new
-    expect(checker).to be_an_instance_of(RangeChecker)
+    expect(@checker).to be_an_instance_of(RangeChecker)
   end
 
   it 'can determine if one range fully contains another' do
-    checker = RangeChecker.new
     a = ["1234", "23"]
     b = ["1234", "5678"]
     c = ["123456", "5678"]
 
-    expect(checker.contained?(a)).to eq(true)
-    expect(checker.contained?(b)).to eq(false)
-    expect(checker.contained?(c)).to eq(false)
+    expect(@checker.contained?(a)).to eq(true)
+    expect(@checker.contained?(b)).to eq(false)
+    expect(@checker.contained?(c)).to eq(false)
+  end
+
+  it 'can read and format txt file' do
+    @checker.format_pair_list(@test)
+    expect(@checker.pair_list).to be_an(Array)
+    expect(@checker.pair_list[0]).to be_an(Array)
+    expect(@checker.pair_list[0][1]).to be_a(String)
   end
 end
