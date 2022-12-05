8d3af7849e48f9ed1882dce2fb14b4ae502e5b58
Sarah Hall <swatson1994@yahoo.com>
2022-12-05 10:50:38 -0500

    Add test for determining full overlap, make pass

--- camp_clean_up/lib/range_checker.rb
+++ camp_clean_up/lib/range_checker.rb
@@ -2,4 +2,10 @@ require 'pry'
 
 class RangeChecker
 
+  def initialize
+  end
+
+  def contained?(array)
+    array[0].include?(array[1]) || array[1].include?(array[0])
+  end
 end
--- camp_clean_up/spec/range_checker_spec.rb
+++ camp_clean_up/spec/range_checker_spec.rb
@@ -1,6 +1,21 @@
 require 'pry'
-require 'spec'
-require_relative './camp_clean_up/lib/range_checker.rb'
+require 'Rspec'
+require_relative '../lib/range_checker.rb'
 
 describe RangeChecker do
+  it 'exists' do
+    checker = RangeChecker.new
+    expect(checker).to be_an_instance_of(RangeChecker)
+  end
+
+  it 'can determine if one range fully contains another' do
+    checker = RangeChecker.new
+    a = ["1234", "23"]
+    b = ["1234", "5678"]
+    c = ["123456", "5678"]
+
+    expect(checker.contained?(a)).to eq(true)
+    expect(checker.contained?(b)).to eq(false)
+    expect(checker.contained?(c)).to eq(false)
+  end
 end
