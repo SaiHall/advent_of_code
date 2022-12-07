2ac728b330d936f8263e7ffb7a6d3eac143b66c8
Sarah Hall <swatson1994@yahoo.com>
2022-12-06 22:15:35 -0500

    Add test for reading and storing of signal, make pass

--- tuning_trouble/lib/data_stream_decode.rb
+++ tuning_trouble/lib/data_stream_decode.rb
@@ -2,7 +2,15 @@ require 'pry'
 
 class DataStreamDecoder
 
+  attr_reader :stream
   def initialize
+    @stream = []
   end
 
+  def read_signal(file_loc)
+    file = File.open(file_loc)
+    file_data = file.read
+    file.close
+    @stream = file_data.chars
+  end
 end
--- tuning_trouble/spec/data_stream_decode_spec.rb
+++ tuning_trouble/spec/data_stream_decode_spec.rb
@@ -3,5 +3,17 @@ require 'Rspec'
 require_relative '../lib/data_stream_decode.rb'
 
 describe DataStreamDecoder do
+  before(:each) do
+    @decoder = DataStreamDecoder.new
+  end
+  it 'exists' do    
 
+    expect(@decoder).to be_an_instance_of(DataStreamDecoder)
+  end
+
+  it 'can read a signal' do
+    @decoder.read_signal('./tuning_trouble/data_stream_buffer.txt')
+    expect(@decoder.stream).to be_an(Array)
+    expect(@decoder.stream.empty?).to eq(false)
+  end
 end
